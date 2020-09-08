1184de65daade7668310a4bb55bc73a7a76140d2
Naman Tuli <63744860+namantuli18@users.noreply.github.com>
2020-09-08 23:33:23 +0530

    Crawler Intelligence Files ink Tokenizer && Model

---
+++ Crawler Intelligence/functions.py
@@ -0,0 +1,26 @@
+import keras
+import os
+import time
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+import warnings
+import pandas as pd
+import pickle
+
+
+SEQUENCE_LENGTH = 300
+tokenizer=Tokenizer()
+
+
+model=keras.models.load_model('crawler_intelligence.h5')
+with open('tokenizer.pkl','rb') as f:
+    tokenizer=pickle.load(f)
+   
+def decode_sentiment(score):
+    return 0 if score < 0.625 else 1
+def predict(text):
+    x_test = pad_sequences(tokenizer.texts_to_sequences([text]), maxlen=SEQUENCE_LENGTH)
+    score = model.predict([x_test])[0]
+    return decode_sentiment(score)
+
