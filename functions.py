7f6b2eeada559441994ac62cbc7778a34bc08d85
bernard0047 <avikkuthiala@gmail.com>
2020-09-12 19:59:13 +0530

    dir fixes

--- functions.py
+++ functions.py
@@ -17,17 +17,17 @@ OUTPUT1='NER Models/Name/content/Model'
 OUTPUT2='NER Models/Prefix/content/Model'
 
 
-# model=keras.models.load_model('crawler_intelligence.h5')
-# with open('tokenizer.pkl','rb') as f:
-#     tokenizer=pickle.load(f)
+model=keras.models.load_model('Crawler Intelligence/crawler_intelligence.h5')
+with open('Crawler Intelligence/tokenizer.pkl','rb') as f:
+    tokenizer=pickle.load(f)
     
 #print("Loading from", OUTPUT_DIR)
 nlpN = spacy.load(OUTPUT1)
 nlpS = spacy.load(OUTPUT2)
-# def decode_sentiment(score):
-#     return 0 if score < 0.625 else 1
-# def predict(text):
-#     x_test = pad_sequences(tokenizer.texts_to_sequences([text]), maxlen=SEQUENCE_LENGTH)
-#     score = model.predict([x_test])[0]
-#     return decode_sentiment(score)
+def decode_sentiment(score):
+    return 0 if score < 0.625 else 1
+def predict(text):
+    x_test = pad_sequences(tokenizer.texts_to_sequences([text]), maxlen=SEQUENCE_LENGTH)
+    score = model.predict([x_test])[0]
+    return decode_sentiment(score)
 
